0142443c0cc316b508f17c0e87f35e6a10d21f04
Thomas Frank <tfrank@ford.com>
2020-03-27 11:03:02 +0100

    V1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,7 +18,7 @@ void MainWindow::on_Text_clicked()
 {
    // QApplication::quit();
 
-    ui->Anzeige->setPlainText("TUeztgghhst");
+    ui->Anzeige->setPlainText("Version1");
 }
 
 void MainWindow::on_Aus_clicked()
