f9c0bc822964de0ad9d63e06da9c002afd81943c
Anna Hoff <anna-hoff@hotmail.com>
2020-03-30 14:50:02 +0200

    commit  merge mit master

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,7 +18,7 @@ void MainWindow::on_Text_clicked()
 {
    // QApplication::quit();
 
-    ui->Anzeige->setPlainText("Anna");
+    ui->Anzeige->setPlainText("Ella");
 }
 
 void MainWindow::on_Aus_clicked()
